2b766d7ca4342438030e20637263579a5dd25a2c
Djavani Gomes <drgomes5@stefanini.com>
2018-03-22 08:50:11 -0300

    Primeiro Commit

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template:`
+    <h1>Angular Running</h1>
+    
+    <contatos-lista></contatos-lista>
+    `
+})
+export class AppComponent {
+
+}
