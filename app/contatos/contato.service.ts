fb84ee6a08fddee6bdc39b138f56ce70ae97933c
djavani <djavanirgomes@gmail.com>
2018-03-23 23:17:28 -0300

    Buscando contato da nossa API

--- app/contatos/contato.service.ts
+++ app/contatos/contato.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from "@angular/core"
+import { Http } from '@angular/http'
+
+import 'rxjs/add/operator/toPromise';
 
 import { CONTATOS } from './contatos-mock';
 import { Contato } from './contato.model';
 @Injectable()
 export class ContatoService {
 
-    getContatos(): Promise<Contato[]> {
-        return Promise.resolve(CONTATOS);
+    private contatosUrl: string = 'app/contatos'
+
+    constructor(
+        private http: Http
+    ){}
+    
+    getContatos(): Promise<Contato[]> {        
+        return this.http.get(this.contatosUrl)
+        .toPromise()
+        .then(response => response.json().data as Contato[]);
+
+        //return Promise.resolve(CONTATOS);
     }
 
     getContato(id: number): Promise<Contato> {
